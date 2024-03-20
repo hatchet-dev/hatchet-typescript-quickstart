bb2dc03d60d1b0642a1233766069dc7381b14eb8
steebchen <contact@luca-steeb.com>
2024-03-21 00:09:34 +0700

    ci(workflows): add basic build workflow

--- simple-examples/src/manual-trigger.ts
+++ simple-examples/src/manual-trigger.ts
@@ -6,6 +6,7 @@ const hatchet = Hatchet.init();
 async function main() {
   const workflowRunId = await hatchet.admin.run_workflow("example", {});
 
+  // @ts-ignore // TODO fix this
   hatchet.listener.on(workflowRunId, async (event: StepRunEvent) => {
     console.log("Received event", event);
   });
