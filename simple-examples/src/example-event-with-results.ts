0b7fbd65462a97f1455b93325b7b09fdcbd022d3
steebchen <contact@luca-steeb.com>
2024-06-28 21:36:09 +0100

    chore(sdk): update sdk; adapt examples

--- simple-examples/src/example-event-with-results.ts
+++ simple-examples/src/example-event-with-results.ts
@@ -3,9 +3,11 @@ import Hatchet from '@hatchet-dev/typescript-sdk';
 async function main() {
   const hatchet = Hatchet.init();
 
-  const workflowRunId = await hatchet.admin.run_workflow('simple-workflow', {
+  const workflowRun = await hatchet.admin.runWorkflow('simple-workflow', {
     test: 'test',
   });
+
+  const workflowRunId = await workflowRun.getWorkflowRunId();
 
   console.log('listening for events');
   for await (const event of await hatchet.listener.stream(workflowRunId)) {
